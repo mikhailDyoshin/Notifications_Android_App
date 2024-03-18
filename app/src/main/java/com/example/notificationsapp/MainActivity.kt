467e3ff8ddfbc6737b943a3b93a601dd95ad1673
misha <mikhail.dyoshin@gmail.com>
2024-03-18 16:19:42 +0300

    Created a basic notification and displayed it using only the MainActivity.

--- app/src/main/java/com/example/notificationsapp/MainActivity.kt
+++ app/src/main/java/com/example/notificationsapp/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.notificationsapp
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,11 +16,67 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.notificationsapp.common.CHANNEL_DESCRIPTION
+import com.example.notificationsapp.common.CHANNEL_NAME
+import com.example.notificationsapp.common.NOTIFICATION_ID
 import com.example.notificationsapp.ui.theme.NotificationsAppTheme
 
 class MainActivity : ComponentActivity() {
+
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = CHANNEL_NAME
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_NAME, name, importance).apply {
+                description = CHANNEL_DESCRIPTION
+            }
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun showNotification(context: Context) {
+
+        with(NotificationManagerCompat.from(this)) {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            notify(NOTIFICATION_ID, builder.build())
+        }
+    }
+
+    private var builder = NotificationCompat.Builder(this, CHANNEL_NAME)
+        .setSmallIcon(androidx.core.R.drawable.notification_bg_normal)
+        .setContentTitle("My notification")
+        .setContentText("Much longer text that cannot fit one line...")
+        .setStyle(NotificationCompat.BigTextStyle()
+            .bigText("Much longer text that cannot fit one line..."))
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        createNotificationChannel()
+        showNotification(this)
+
         setContent {
             NotificationsAppTheme {
                 // A surface container using the 'background' color from the theme
---
+++ app/src/main/java/com/example/notificationsapp/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.notificationsapp.common
+
+const val CHANNEL_NAME = "BasicNotificationsChannel"
+const val CHANNEL_DESCRIPTION = "Channel for basic notifications"
+const val NOTIFICATION_ID = 1045
