175062e578a445e96c47ef36bc3ce930b4d1f005
misha <mikhail.dyoshin@gmail.com>
2024-03-19 16:01:37 +0300

    Created the NotificationsLauncherScreen-composable to launch notifications manually.

---
+++ app/src/main/java/com/example/notificationsapp/presentation/NotificationsLauncherScreen.kt
@@ -0,0 +1,41 @@
+package com.example.notificationsapp.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NotificationsLauncherScreen(notifyBasic: () -> Unit, notifyReply: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+            Button(onClick = { notifyBasic() }) {
+                Text(text = "Basic")
+            }
+            Button(onClick = { notifyReply() }) {
+                Text(text = "Reply")
+            }
+        }
+
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun NotificationsLauncherScreenPreview() {
+    NotificationsLauncherScreen(
+        notifyBasic = {},
+        notifyReply = {},
+    )
+}
