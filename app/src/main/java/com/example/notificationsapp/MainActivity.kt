aa6a7f4a7e17659ec257f43e7aef14c7c914514a
misha <mikhail.dyoshin@gmail.com>
2024-03-19 16:23:14 +0300

    Created constants for notifications' IDs

--- app/src/main/java/com/example/notificationsapp/MainActivity.kt
+++ app/src/main/java/com/example/notificationsapp/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.notificationsapp
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import com.example.notificationsapp.common.NOTIFICATION_ID
+import com.example.notificationsapp.common.BASIC_NOTIFICATION_ID
+import com.example.notificationsapp.common.REPLY_NOTIFICATION_ID
 import com.example.notificationsapp.notifications.NotificationHelper
 import com.example.notificationsapp.notifications.NotificationType
 import com.example.notificationsapp.notifications.NotificationsStore
@@ -28,8 +21,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val basicNotification = NotificationsStore().getNotification(context, NotificationType.BASIC)
-        val replyNotification = NotificationsStore().getNotification(context, NotificationType.REPLY)
+        val basicNotification =
+            NotificationsStore().getNotification(context, NotificationType.BASIC)
+        val replyNotification =
+            NotificationsStore().getNotification(context, NotificationType.REPLY)
 
         setContent {
             NotificationsAppTheme {
@@ -39,8 +34,20 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     NotificationsLauncherScreen(
-                        notifyBasic = { NotificationHelper.notify(context, 11, basicNotification) },
-                        notifyReply = { NotificationHelper.notify(context, 15, replyNotification) }
+                        notifyBasic = {
+                            NotificationHelper.notify(
+                                context,
+                                BASIC_NOTIFICATION_ID,
+                                basicNotification
+                            )
+                        },
+                        notifyReply = {
+                            NotificationHelper.notify(
+                                context,
+                                REPLY_NOTIFICATION_ID,
+                                replyNotification
+                            )
+                        }
                     )
                 }
             }
--- app/src/main/java/com/example/notificationsapp/broadcastReceiver/ReplyNotificationReceiver.kt
+++ app/src/main/java/com/example/notificationsapp/broadcastReceiver/ReplyNotificationReceiver.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.RemoteInput
 import com.example.notificationsapp.common.KEY_TEXT_REPLY
-import com.example.notificationsapp.common.NOTIFICATION_ID
+import com.example.notificationsapp.common.REPLY_NOTIFICATION_ID
 
 class ReplyNotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
@@ -18,7 +18,7 @@ class ReplyNotificationReceiver : BroadcastReceiver() {
                 KEY_TEXT_REPLY
             ).toString()
             Toast.makeText(context, title, Toast.LENGTH_SHORT).show()
-            NotificationManagerCompat.from(context).cancel(NOTIFICATION_ID)
+            NotificationManagerCompat.from(context).cancel(REPLY_NOTIFICATION_ID)
         }
     }
 }
