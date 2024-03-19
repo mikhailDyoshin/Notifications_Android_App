3a0898097d97d28db99589f5bb4d5c0cb03cd248
misha <mikhail.dyoshin@gmail.com>
2024-03-19 09:51:47 +0300

    Created a logic to launch an activity from notification.

--- app/src/main/java/com/example/notificationsapp/notifications/NotificationsStore.kt
+++ app/src/main/java/com/example/notificationsapp/notifications/NotificationsStore.kt
@@ -1,12 +1,21 @@
 package com.example.notificationsapp.notifications
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.example.notificationsapp.MainActivity
 import com.example.notificationsapp.common.CHANNEL_NAME
 
 class NotificationsStore {
 
     fun getNotification(context: Context): NotificationCompat.Builder {
+
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
         return NotificationCompat.Builder(context, CHANNEL_NAME)
             .setSmallIcon(androidx.core.R.drawable.notification_bg_normal)
             .setContentTitle("My notification")
@@ -16,6 +25,8 @@ class NotificationsStore {
                     .bigText("Much longer text that cannot fit one line. Hello, here is the second line of the text.")
             )
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
     }
 
 }
