7ade94658c7389cf67ebd6f5fde80f1fb58b2fd1
Your Name <you@example.com>
2024-04-08 16:25:53 +0300

    Created a notification with media controls used by media-session.
    Controls are not working now.

--- app/src/main/java/com/example/notificationsapp/service/PlaybackService.kt
+++ app/src/main/java/com/example/notificationsapp/service/PlaybackService.kt
@@ -1,18 +1,57 @@
 package com.example.notificationsapp.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.OptIn
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.CommandButton
+import androidx.media3.session.MediaNotification
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import androidx.media3.session.MediaStyleNotificationHelper
+import com.google.common.collect.ImmutableList
 
 class PlaybackService : MediaSessionService() {
     private var mediaSession: MediaSession? = null
 
+    private lateinit var nBuilder: NotificationCompat.Builder
+
     // Create your player and media session in the onCreate lifecycle event
+    @OptIn(UnstableApi::class)
     override fun onCreate() {
         super.onCreate()
         val player = ExoPlayer.Builder(this).build()
         mediaSession = MediaSession.Builder(this, player).build()
+
+        this.setMediaNotificationProvider(object : MediaNotification.Provider {
+            @RequiresApi(Build.VERSION_CODES.O)
+            override fun createNotification(
+                mediaSession: MediaSession,// this is the session we pass to style
+                customLayout: ImmutableList<CommandButton>,
+                actionFactory: MediaNotification.ActionFactory,
+                onNotificationChangedCallback: MediaNotification.Provider.Callback
+            ): MediaNotification {
+                createNotification(mediaSession)
+                // notification should be created before you return here
+                return MediaNotification(123, nBuilder.build())
+            }
+
+            override fun handleCustomCommand(
+                session: MediaSession,
+                action: String,
+                extras: Bundle
+            ): Boolean {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     // The user dismissed the app from the recent tasks
@@ -25,7 +64,8 @@ class PlaybackService : MediaSessionService() {
         }
     }
 
-    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? = mediaSession
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? =
+        mediaSession
 
     // Remember to release the player and media session in onDestroy
     override fun onDestroy() {
@@ -35,5 +75,64 @@ class PlaybackService : MediaSessionService() {
             mediaSession = null
         }
         super.onDestroy()
+    }
+
+    @OptIn(UnstableApi::class)
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createNotification(session: MediaSession) {
+
+        val repeatPendingIntent: PendingIntent? = null
+        val prevPendingIntent: PendingIntent? = null
+        val pausePendingIntent: PendingIntent? = null
+        val nextPendingIntent: PendingIntent? = null
+        val controlSpeed: PendingIntent? = null
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(
+            NotificationChannel(
+                CHANNEL_ID,
+                "Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+        )
+
+        // NotificationCompat.Builder here.
+        nBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setSmallIcon(androidx.media3.ui.R.drawable.exo_icon_vr)
+            .addAction(
+                androidx.media3.ui.R.drawable.exo_icon_repeat_all,
+                "Repeat all",
+                repeatPendingIntent
+            )
+            .addAction(
+                androidx.media3.ui.R.drawable.exo_notification_previous,
+                "Previous",
+                prevPendingIntent
+            ) // #0
+            .addAction(
+                androidx.media3.ui.R.drawable.exo_notification_pause,
+                "Pause",
+                pausePendingIntent
+            ) // #1
+            .addAction(
+                androidx.media3.ui.R.drawable.exo_notification_next,
+                "Next",
+                nextPendingIntent
+            ) // #2
+            .addAction(
+                androidx.media3.ui.R.drawable.exo_styled_controls_speed,
+                "Control speed",
+                controlSpeed
+            )
+            .setStyle(
+                MediaStyleNotificationHelper.MediaStyle(session)
+                    .setShowActionsInCompactView(2 /* #1: pause button \*/)
+            )
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "PlaybackServiceChannel"
     }
 }
